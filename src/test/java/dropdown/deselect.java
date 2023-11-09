ccf63b21f1860c5809fca122dbfc934c14cf7236
BudagamPreethi <150000488+BudagamPreethi@users.noreply.github.com>
2023-11-09 11:30:17 +0530

    Update deselect.java

--- src/test/java/dropdown/deselect.java
+++ src/test/java/dropdown/deselect.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-//done sucessfully
+//done sucessfully completed
 public class deselect {
 
 	public static void main(String[] args) throws InterruptedException {
